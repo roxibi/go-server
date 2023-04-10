f2231844604bc94736636f4c58f025427f781fe9
Roxana Bita <roxana.bita@gmail.com>
2023-04-10 11:27:31 +0200

    COMMENT code explanation

--- main.go
+++ main.go
@@ -2,46 +2,48 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-func formHandler (w http.ResponseWriter, r *http.Request) {
-if err := r.ParseForm(); err != nil {
-	fmt.Fprint(w, "ParseForm() err:%v", err)
-	return
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Print(w, "ParseForm() err:%v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST req successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name =%s \n", name)
+	fmt.Fprintf(w, "Address = %s \n", address)
 }
-fmt.Fprintf(w, "POST req successful")
-name := r.FormValue("name")
-address := r.FormValue("address")
-fmt.Fprintf(w, "Name =%s \n", name)
-fmt.Fprintf(w, "Address = %s \n", address)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path !="/hello" {
+// HANDLER HAS A RESPONSE AND A REQUEST AS ARGUMENTS
+// IF THE PATH OR THE METHOD DEFINED FOR THIS HANDLER ARE INCORRECT, WE RETURN ERRORS FOR THAT; OTHERWISE WE JUST EXECUTE THE HANDLER
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
 
-	if r.Method !="GET"{
+	if r.Method != "GET" {
 		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
-fmt.Fprintf(w, "hello")
+	fmt.Fprintf(w, "hello")
 }
 
+func main() {
+	fileServer := http.FileServer(http.Dir("./static")) //TELL GO WHERE TO FIND INDEX HTML
+	http.Handle("/", fileServer)                        // ROOT ROUTE-NOT A SIMPLE FUNC
+	http.HandleFunc("/form", formHandler)               // OTHER ROUTES
+	http.HandleFunc("/hello", helloHandler)             // PARAMS=ROUTE AND HANDLER FUNCTION
 
-func main(){
-fileServer := http.FileServer(http.Dir("./static"))
-http.Handle("/", fileServer)
-http.HandleFunc("/form", formHandler)
-http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Server started at port 8080 \n") //TEST SERVER
 
-fmt.Printf("Server started at port 8080 \n")
-
-if err := http.ListenAndServe(":8080", nil); err !=nil {
-	log.Fatal(err)
-}
+	//LISTEN AND SERVE DOESNT RETURN ANYTHING UNLESS IT'S AN ERROR - IT JUST STARTS TO RUN IN THE BACKGROUND; SO WE SAVE THE ERROR IN err AND PRINT IT IF IT'S NOT NIL
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
